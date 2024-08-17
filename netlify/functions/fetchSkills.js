2bbc09975268a557b6d4db559f0534a26ce26756
bychrisgomez <christian@vantage-communications.com>
2024-08-17 11:49:18 -0400

    node cache

--- netlify/functions/fetchSkills.js
+++ netlify/functions/fetchSkills.js
@@ -1,15 +1,26 @@
 const axios = require('axios');
+const NodeCache = require('node-cache');
+
+const cache = new NodeCache({ stdTTL: 3600 }); //cache for 1 hour
 
 exports.handler = async function(event, context) {
   try {
-    const response = await axios.get('https://docs.google.com/spreadsheets/d/10YZra-FKNCClwlBpGJ8w-O_nikQ3GmMnlzlWcxi-1rg/pub?gid=1076834980&single=true&output=csv');
+    const cacheKey = 'skillsData';
+    let data = cache.get(cacheKey);
+
+    if (data === undefined){
+        const response = await axios.get('https://docs.google.com/spreadsheets/d/10YZra-FKNCClwlBpGJ8w-O_nikQ3GmMnlzlWcxi-1rg/pub?gid=1076834980&single=true&output=csv');
+        data = response.data;
+        cache.set(cacheKey, data);
+    }
+    
     return {
       statusCode: 200,
       headers: {
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'text/csv'
       },
-      body: response.data
+      body: data
     };
   } catch (error) {
     return {
